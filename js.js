d0d216470b567e371ee6ba27a1f368faaabf878b
Dmitry Sivkov <dimm1975@gmail.com>
2024-11-18 18:04:37 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('Never imagine yourself not to be otherwise than what it might appear to others that what you were or might have been was not otherwise than what you had been would have appeared to them to be otherwise.')
+})
